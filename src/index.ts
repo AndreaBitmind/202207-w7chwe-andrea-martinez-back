4f5ad51c0d2f8bc4c825b34b3ca7806fa055a463
Andrea Martinez <andrea.maort@gmail.com>
2022-08-19 22:12:42 +0200

    added Environment and conexion to mongo database

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import "./loadEnvironment";
 
+const port = +process.env.PORT || 4000;
+const mongoURL = process.env.MONGO_URL;
+
+(async () => {
+  try {
+    await connectDB(mongoURL);
+    await startServer(port);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
