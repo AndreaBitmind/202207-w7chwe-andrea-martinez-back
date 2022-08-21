c25401990216d16604cbe1918a763812bacaeba1
Andrea Martinez <andrea.maort@gmail.com>
2022-08-21 16:44:09 +0200

    added login user controller

--- src/interfaces/UserInterfaces.ts
+++ src/interfaces/UserInterfaces.ts
@@ -3,3 +3,14 @@ export interface UserRegister {
   password: string;
   image: string;
 }
+
+export interface CustomJwTPayload {
+  id: string;
+  userName: string;
+}
+
+export interface UserLogin {
+  id: string;
+  userName: string;
+  password: string;
+}
--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,8 +1,15 @@
+import "../loadEnvironment";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import { CustomJwTPayload } from "../interfaces/UserInterfaces";
 
-const hashCreator = (text: string) => {
+export const tokenCreator = (payload: CustomJwTPayload) =>
+  jwt.sign(payload, process.env.SECRET);
+
+export const hashCreator = (text: string) => {
   const salt = 10;
   return bcrypt.hash(text, salt);
 };
 
-export default hashCreator;
+export const hashCompare = (text: string, hash: string) =>
+  bcrypt.compare(text, hash);
